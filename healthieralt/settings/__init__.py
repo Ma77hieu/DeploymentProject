b587c2ee99fb1c3b04169765b2e98825224f0f03
Matthieu O <mat.ortlieb@gmail.com>
2021-06-29 16:04:39 +0000

    travis now has access to the env variable django key

--- healthieralt/settings/__init__.py
+++ healthieralt/settings/__init__.py
@@ -22,10 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.environ.get('DJANGO_KEY') is not None:
-    SECRET_KEY = os.environ.get('DJANGO_KEY')
-else:
-    SECRET_KEY = config("DJANGO_KEY")
+SECRET_KEY = config("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
